1b5b39bfae1eba0849e58f8352ac69963a156e89
Malia Cortez <mookiethompson17@gmail.com>
2020-01-17 18:07:07 -0800

    Add completed chapter 7 exercise

--- chapter-07-exercises/exercise-2/exercise.R
+++ chapter-07-exercises/exercise-2/exercise.R
@@ -2,47 +2,64 @@
 
 # Create a vector `first_ten` that has the values 10 through 20 in it (using 
 # the : operator)
-
+first_ten <- 10:20
 
 # Create a vector `next_ten` that has the values 21 through 30 in it (using the 
 # seq() function)
+next_ten <- seq(21, 30)
 
 
 # Create a vector `all_numbers` by combining the previous two vectors
+all_numbers <- next_ten + first_ten
 
 
 # Create a variable `eleventh` that contains the 11th element in `all_numbers`
+eleventh <- all_numbers[11]
 
 
 # Create a vector `some_numbers` that contains the 2nd through the 5th elements 
 # of `all_numbers`
+some_numbers <- c(all_numbers[2], all_numbers[3], all_numbers[4], all_numbers[5])
 
 
 
 # Create a vector `even` that holds the even numbers from 1 to 100
+even <- seq(2, 100, 2)
 
 
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
-
+even_nums <- c(even %% 2 == 0)
+all(even_nums)
 
 
 # Create a vector `phone_numbers` that contains the numbers 8, 6, 7, 5, 3, 0, 9
+phone_numbers <- c(8, 6, 7, 5, 3, 0, 9)
 
 
 # Create a vector `prefix` that has the first three elements of `phone_numbers`
+prefix <- c(phone_numbers[1], phone_numbers[2], phone_numbers[3])
 
 
 # Create a vector `small` that has the values of `phone_numbers` that are 
 # less than or equal to 5
+small <- c(phone_numbers <= 5)
+print(small)
 
 
 # Create a vector `large` that has the values of `phone_numbers` that are 
 # strictly greater than 5
+large <- c(phone_numbers[phone_numbers > 6])
+print(large)
 
 
 # Replace the values in `phone_numbers` that are larger than 5 with the number 5
+greater_than_5 <- phone_numbers[phone_numbers > 5]
+phone_numbers <- greater_than_5
+print(phone_numbers)
 
 
 # Replace every odd-numbered value in `phone_numbers` with the number 0
+odd_numbers <- phone_numbers[phone_numbers %% 2 == 1]
+print(odd_numbers)
 
